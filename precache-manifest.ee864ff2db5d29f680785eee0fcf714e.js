e84027ba19eac111794fdecac5b9945d2fb940f7
joshuatac <40856365+joshuatac@users.noreply.github.com>
2020-12-11 13:44:58 +0100

    Initial commit

---
+++ precache-manifest.ee864ff2db5d29f680785eee0fcf714e.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4254441801f140769872bac73adb06b7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e1179289efb02f89c17b",
+    "url": "/static/css/main.1923fc07.chunk.css"
+  },
+  {
+    "revision": "51bd9e93870d5d24d374",
+    "url": "/static/js/2.4e940891.chunk.js"
+  },
+  {
+    "revision": "5f8be6edcb631ea3c834b8459dc2c264",
+    "url": "/static/js/2.4e940891.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e1179289efb02f89c17b",
+    "url": "/static/js/main.efadcf65.chunk.js"
+  },
+  {
+    "revision": "92a146e5ab9e0f9e785b",
+    "url": "/static/js/runtime-main.90140090.js"
+  },
+  {
+    "revision": "f1b7fec6c618702caaa9a8aecf733608",
+    "url": "/static/media/amazon logo.f1b7fec6.png"
+  },
+  {
+    "revision": "eafab5dc4294fca2cf2b25f99736d937",
+    "url": "/static/media/banner.eafab5dc.jpg"
+  },
+  {
+    "revision": "580078d2ac7425762db224ab4be673bf",
+    "url": "/static/media/checkout_banner.580078d2.jpg"
+  }
+]);
